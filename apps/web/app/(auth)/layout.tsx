074b27274c5bb24aab45d88d6128cd34928db861
Maneesh Kolli <maneesh.kolli@gmail.com>
2025-11-17 20:21:43 -0500

    removed page header

--- apps/web/app/(auth)/layout.tsx
+++ apps/web/app/(auth)/layout.tsx
@@ -21,14 +21,6 @@ export default async function AuthLayout({
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <div className="flex flex-1 items-center gap-2">
-            <h1 className="text-lg font-semibold">
-              Welcome, {session.user?.name}
-            </h1>
-          </div>
-        </header>
         <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
       </SidebarInset>
     </SidebarProvider>
